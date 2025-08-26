063dc6d759c2b97562ff7bf3beeccd6ddd201f56
infoenum <infoenum.testing@gmail.com>
2025-08-26 19:12:51 +0530

    Added Splash screen in android

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,16 +1,22 @@
 // AppNavigator.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from '../screens/OnboardingScreen';
 import CreateAccountScreen from '../screens/CreateAccountScreen';
 import HomeScreen from '../screens/HomeScreen';
 import ProductDetailScreen from '../screens/ProductDetailScreen';
+import SplashScreen from 'react-native-splash-screen';
 import { ROUTES } from '../helper/routes';
 
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
+
+  useEffect(() => {
+    SplashScreen.hide(); 
+  }, []);
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name={ROUTES.ONBOARDING} component={OnboardingScreen} />
