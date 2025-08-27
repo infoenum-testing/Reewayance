871cdac3187de10bc14cc32be4444064b66e9036
infoenum <infoenum.testing@gmail.com>
2025-08-27 11:26:51 +0530

    Fix Bug

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -7,7 +7,7 @@ import HomeScreen from '../screens/HomeScreen';
 import ProductDetailScreen from '../screens/ProductDetailScreen';
 import LoginScreen from '../screens/LoginScreen';
 import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
-import VarificationScreen from '../screens/VarificationScreen';
+import VarificationCodeScreen from '../screens/VarificationCodeScreen';
 import ResetPasswordScreen from '../screens/ResetPasswordScreen';
 import SplashScreen from 'react-native-splash-screen';
 import { ROUTES } from '../helper/routes';
@@ -15,22 +15,38 @@ import { ROUTES } from '../helper/routes';
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
-    return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name={ROUTES.ONBOARDING} component={OnboardingScreen} />
-            <Stack.Screen name={ROUTES.CREATE_ACCOUNT} component={CreateAccountScreen} />
-            <Stack.Screen name={ROUTES.Login_SCREEN} component={LoginScreen} />
-            <Stack.Screen name={ROUTES.FORGOT_PASSWORD} component={ForgotPasswordScreen} />
-            <Stack.Screen name={ROUTES.Varification_SCREEN} component={VarificationScreen} />
-            <Stack.Screen name={ROUTES.RESET_PASSWORD} component={ResetPasswordScreen} />
-            <Stack.Screen name={ROUTES.HOMESCREEN} component={HomeScreen} />
-            <Stack.Screen name={ROUTES.PRODUCT_DETAIL} component={ProductDetailScreen} />
-        </Stack.Navigator>
-    );
-
+  
   useEffect(() => {
     SplashScreen.hide();
   }, []);
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name={ROUTES.ONBOARDING} component={OnboardingScreen} />
+      <Stack.Screen
+        name={ROUTES.CREATE_ACCOUNT}
+        component={CreateAccountScreen}
+      />
+      <Stack.Screen name={ROUTES.Login_SCREEN} component={LoginScreen} />
+      <Stack.Screen
+        name={ROUTES.FORGOT_PASSWORD}
+        component={ForgotPasswordScreen}
+      />
+      <Stack.Screen
+        name={ROUTES.Varification_SCREEN}
+        component={VarificationCodeScreen}
+      />
+      <Stack.Screen
+        name={ROUTES.RESET_PASSWORD}
+        component={ResetPasswordScreen}
+      />
+      <Stack.Screen name={ROUTES.HOMESCREEN} component={HomeScreen} />
+      <Stack.Screen
+        name={ROUTES.PRODUCT_DETAIL}
+        component={ProductDetailScreen}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default AppNavigator;
