874d18d264077966ca406224468c887e0b564bb0
infoenum <infoenum.testing@gmail.com>
2025-09-03 17:09:21 +0530

    update ui of homeScreen

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+import { ROUTES } from '../helper/routes';
+const Icons = {
+  back: require('../assets/backButtonImage.png'),
+  bell: require('../assets/images/vector.png'),
+};
 
-const Header = ({ title, rightIcon }) => (
+const Header = ({ title }) => {
+const navigation = useNavigation();
+  return (
   <View style={styles.header}>
     <Text style={styles.headerTitle}>{title}</Text>
-    {rightIcon && <Image source={rightIcon} style={styles.headerIcon} />}
+         <TouchableOpacity
+           onPress={() => navigation.navigate(ROUTES.Notification_Screen)}
+         >
+           <Image source={Icons.bell} style={styles.headerIcon} />
+         </TouchableOpacity>
   </View>
-);
+  );
+};
 
 export default Header;
 
